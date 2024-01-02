c39c3dc5af6c6a46ec60446a83179da17094903b
Manuel Hutter <manuel@hutter.io>
2024-01-02 21:05:46 +0100

    WIP restructure
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- configuration.nix
+++ configuration.nix
@@ -1,60 +1,34 @@
 { config, lib, pkgs, ... }:
 
+let
+  # Network settings
+  hostname = "rhea";
+  nic = "enp35s0";
+  ipv4 = {
+    address = "116.202.233.38";
+    gateway = "116.202.233.1";
+    prefixLength = 26;
+    netmask = "255.255.255.192";
+  };
+  ipv6 = {
+    address = "2a01:4f8:241:4c27::1";
+    prefixLength = 64;
+    gateway = "fe80::1";
+  };
+in
 {
-  imports = [ ./hardware-configuration.nix ];
+  imports = [
+    ./hardware-configuration.nix
+  ];
 
-  boot.loader.grub = {
-    enable = true;
-    device = "/dev/nvme0n1";
-    mirroredBoots = [
-      {
-        devices = [ "/dev/nvme1n1" ];
-        path = "/boot-fallback";
-      }
-    ];
-  };
-  boot.initrd.network = {
-    enable = true;
-    ssh = {
-      enable = true;
-      port = 41083;
-      # this is the default
-      # authorizedKeys = config.users.users.root.openssh.authorizedKeys.keys;
-      hostKeys = [ "/nix/secret/initrd/ssh_host_ed25519_key" ];
-    };
+  boot = (import ./boot.nix) { inherit config ipv4 nic; };
+  networking = (import ./networking.nix) {
+    inherit config ipv4 ipv6 hostname nic;
   };
 
-  networking.hostName = "rhea"; # Define your hostname.
-  # set to `boot.swraid.mdadmConf` by default
-  # environment.etc."mdadm.conf".text = ''
-  #   HOMEHOST rhea
-  # '';
+  time. timeZone = "UTC";
 
-  boot.swraid.enable = true;
-  boot.swraid.mdadmConf = ''
-    HOMEHOST rhea
-  '';
-
-  boot.kernelParams = [ "ip=116.202.233.38::116.202.233.1:255.255.255.192:rhea:enp35s0:off" ];
-  networking = {
-    useDHCP = false;
-    interfaces."enp35s0" = {
-      ipv4.addresses = [{ address = "116.202.233.38"; prefixLength = 26; }];
-      ipv6.addresses = [{ address = "2a01:4f8:241:4c27::1"; prefixLength = 64; }];
-    };
-    defaultGateway = "116.202.233.1";
-    defaultGateway6 = { address = "fe80::1"; interface = "enp35s0"; };
-    nameservers = [
-      "1.1.1.1"
-      "8.8.8.8"
-      "2606:4700:4700::1111"
-      "2001:4860:4860::8844"
-    ];
-  };
-
-  time.timeZone = "UTC";
-
-  users.mutableUsers = false;
+  users. mutableUsers = false;
   users.users.root = {
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILRFlkyW0MXxYjA1HUzJ18nlTLtXOHKV0rVJD/46v7Sb tera2023" ];
   };
@@ -69,11 +43,12 @@
     enable = true;
     ports = [ 6272 ];
     settings.PermitRootLogin = "prohibit-password";
+    # prevent RSA host key from happening
+    hostKeys = [{
+      path = "/etc/ssh/ssh_host_ed25519_key";
+      type = "ed25519";
+    }];
   };
-
-  # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ ] ++ config.services.openssh.ports;
-  system.stateVersion = "24.05"; # Did you read the comment?
 
   # persistence
   environment.etc."ssh/ssh_host_rsa_key".source = "/nix/persist/etc/ssh/ssh_host_rsa_key";
@@ -82,4 +57,6 @@
   environment.etc."ssh/ssh_host_ed25519_key.pub".source = "/nix/persist/etc/ssh/ssh_host_ed25519_key.pub";
 
   environment.etc."machine-id".source = "/nix/persist/etc/machine-id";
+
+  system.stateVersion = "24.05"; # Did you read the comment?
 }
