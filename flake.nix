fe91fd0cf540753a693b5ffc39433013c2355edd
Manuel Hutter <manuel@hutter.io>
2024-01-10 09:42:58 +0100

    Use deploy-rs for deployment
    While `nixos-rebuild` does the job, it does not offer advanced features
    like magic rollbacks.
    
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- flake.nix
+++ flake.nix
@@ -1,19 +1,44 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  description = "Deployment for my server(s)";
 
-  outputs = { self, nixpkgs }:
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    deploy-rs = {
+      url = "github:serokell/deploy-rs";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, deploy-rs }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in
     {
-      devShell."${system}" = pkgs.mkShell {
-        packages = with pkgs; [ nixos-rebuild ];
+      # Ensure necessary tooling is available
+      devShells."${system}".default = pkgs.mkShell {
+        packages = [ pkgs.deploy-rs ];
       };
 
+      # NixOS configurations
       nixosConfigurations.rhea = nixpkgs.lib.nixosSystem {
-        inherit system;
+        system = "x86_64-linux ";
         modules = [ ./configuration.nix ];
       };
+
+      # Deployment specifications
+      deploy.nodes.rhea = {
+        hostname = "rhea";
+        sshUser = "root";
+        remoteBuild = true;
+
+        profiles.system = {
+          path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.rhea;
+        };
+      };
+
+      # FIXME: reenable; will pull a gig of dependencies ...
+      # checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
     };
 }
+
