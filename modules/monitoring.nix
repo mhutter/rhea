fd1e8356a05376192325d8d366d82d1d7460bbd2
Manuel Hutter <manuel@hutter.io>
2024-02-27 13:11:46 +0100

    Deploy VictoriaMetrics
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- modules/monitoring.nix
+++ modules/monitoring.nix
@@ -1,13 +1,20 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 
 let
   # Configuration
   grafanaDomain = "grafana.mhnet.app";
 
+  # Helpers
+  gss = config.services.grafana.settings.server;
+  cfgGrafanaAddr = "${gss.http_addr}:${toString gss.http_port}";
+  lss = config.services.loki.configuration.server;
+  cfgLokiAddr = "${lss.http_listen_address}:${toString lss.http_listen_port}";
+  cfgVickyAddr = config.services.victoriametrics.listenAddress;
+
 in
 {
   services.caddy.virtualHosts."${grafanaDomain}".extraConfig = ''
-    reverse_proxy 127.0.0.1:${toString config.services.grafana.settings.server.http_port}
+    reverse_proxy ${cfgGrafanaAddr}
   '';
 
   services.grafana = {
@@ -30,12 +37,21 @@ in
 
     provision = {
       enable = true;
-      datasources.settings.datasources = [{
-        name = "Loki";
-        type = "loki";
-        access = "proxy";
-        url = "http://127.0.0.1:${toString config.services.loki.configuration.server.http_listen_port}";
-      }];
+      datasources.settings.datasources = [
+        {
+          name = "Loki";
+          type = "loki";
+          access = "proxy";
+          url = "http://${cfgLokiAddr}";
+        }
+        {
+          name = "VictoriaMetrics";
+          type = "prometheus";
+          access = "proxy";
+          url = "http://${cfgVickyAddr}";
+          isDefault = true;
+        }
+      ];
     };
   };
 
@@ -45,12 +61,18 @@ in
       auth_enabled = false;
       analytics.reporting_enabled = false;
 
-      server.http_listen_port = 3100;
+      server = {
+        http_listen_address = "127.0.0.1";
+        http_listen_port = 3100;
+        grpc_listen_address = "127.0.0.1";
+        grpc_listen_port = 9095;
+      };
       memberlist.join_members = [ "rhea:7946" ];
 
       common = {
-        replication_factor = 1;
+        instance_addr = "127.0.0.1";
         path_prefix = config.services.loki.dataDir;
+        replication_factor = 1;
         ring.kvstore.store = "inmemory";
       };
 
@@ -71,14 +93,16 @@ in
     enable = true;
     configuration = {
       server = {
+        http_listen_address = "127.0.0.1";
         http_listen_port = 3031;
+        grpc_listen_address = "127.0.0.1";
         grpc_listen_port = 0;
       };
       positions = {
         filename = "/tmp/positions.yaml";
       };
       clients = [{
-        url = "http://127.0.0.1:${toString config.services.loki.configuration.server.http_listen_port}/loki/api/v1/push";
+        url = "http://${cfgLokiAddr}/loki/api/v1/push";
       }];
       scrape_configs = [{
         job_name = "journal";
@@ -97,7 +121,41 @@ in
     };
   };
 
+  services.caddy.virtualHosts."victoriametrics.mhnet.app".extraConfig = ''
+    reverse_proxy ${cfgVickyAddr}
+    basicauth {
+      mh $2a$14$aDHvtUSsdfIQXab8FpZGduzxcjAwAR.maIMOUeIstCpInVyBbLAJi
+    }
+  '';
+  services.victoriametrics = {
+    enable = true;
+    listenAddress = "127.0.0.1:8428";
+    retentionPeriod = 12; # months; must be a number
+    # storageDataPath is hardcoded to /var/lib/victoriametrics
+    extraOptions = [
+      "-promscrape.config=${pkgs.writeText "scrape.yml" ''
+global:
+  scrape_interval: 15s
+
+scrape_configs:
+  - job_name: grafana
+    static_configs:
+      - targets:
+          - http://${cfgGrafanaAddr}/metrics
+  - job_name: loki
+    static_configs:
+      - targets:
+          - http://${cfgLokiAddr}/metrics
+  - job_name: victoriametrics
+    static_configs:
+      - targets:
+          - http://${cfgVickyAddr}/metrics
+''}"
+    ];
+  };
+
   modules.persistence.dirs = {
+    "/var/lib/private/victoriametrics" = { };
     "${config.services.loki.dataDir}" = { inherit (config.services.loki) user group; };
     "${config.services.grafana.dataDir}" = {
       user = config.systemd.services.grafana.serviceConfig.User;
