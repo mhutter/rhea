6d09dbef653e881fef5c0f4cb6fff12f08e06404
Manuel Hutter <manuel@hutter.io>
2024-03-13 07:58:39 +0100

    Refactor secrets handling a bit
    - Move `secrets.nix` to the Repository root so I don't have to `cd
      secrets` every time I want to edit a secret
    - DRY up the code in `secrets.nix` a bit
    
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

---
+++ secrets.nix
@@ -0,0 +1,24 @@
+let
+  # Users
+  mh = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOPhRxDZsJ7zFb7Zz7vrRMmIvptWCfA2HgnxYnlmhu24";
+
+  # Systems
+  rhea = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBhHDtoGETYvm8EleeFSTRTDnCQRLF9LL+tOaqDnNxzv";
+
+  toSecret = file: {
+    name = "secrets/${file}";
+    value = { publicKeys = [ mh rhea ]; };
+  };
+
+in
+builtins.listToAttrs (builtins.map toSecret [
+  "docspell-env.age"
+  "grafana-env.age"
+  "miniflux-env.age"
+  "postgresql-init.age"
+  "rauthy.age"
+  "restic-env.age"
+  "restic-password.age"
+  "restic-repository.age"
+  "vaultwarden.age"
+])
