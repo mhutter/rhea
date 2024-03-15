8e15a3da61a7302145f3eb1308762659a80ff92d
Manuel Hutter <manuel@hutter.io>
2024-03-15 14:58:21 +0100

    wireguard: trust all traffic from wg0
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- services/wireguard.nix
+++ services/wireguard.nix
@@ -3,13 +3,12 @@
 let
   serverPort = 45516;
   serverInterface = config.modules.networking.nic;
+  wgNic = "wg0";
 
   ip4tables = "${pkgs.iptables}/bin/iptables";
   ip6tables = "${pkgs.iptables}/bin/ip6tables";
   nat4Rule = "POSTROUTING -s 10.10.10.0/24 -o ${serverInterface} -j MASQUERADE";
-  input4Rule = "INPUT -i wg0 -s 10.10.10.0/24 -j ACCEPT";
   nat6Rule = "POSTROUTING -s fd3f:3ca6:ffc1::1/64 -o ${serverInterface} -j MASQUERADE";
-  input6Rule = "INPUT -i wg0 -s fd3f:3ca6:ffc1::1/64 -j ACCEPT";
 
 in
 {
@@ -19,29 +18,26 @@ in
     nat = {
       enable = true;
       externalInterface = serverInterface;
-      internalInterfaces = [ "wg0" ];
+      internalInterfaces = [ wgNic ];
     };
 
     firewall.allowedUDPPorts = [ serverPort ];
+    firewall.trustedInterfaces = [ wgNic ];
 
-    wireguard.interfaces.wg0 = {
+    wireguard.interfaces."${wgNic}" = {
       ips = [ "10.10.10.1/24" "fd3f:3ca6:ffc1::1/64" ];
       listenPort = serverPort;
 
       postSetup = [
         "${ip4tables} -t nat -I ${nat4Rule}"
-        "${ip4tables} -t filter -I ${input4Rule}"
         "${ip6tables} -t nat -I ${nat6Rule}"
-        "${ip6tables} -t filter -I ${input6Rule}"
       ];
       postShutdown = [
         "${ip4tables} -t nat -D ${nat4Rule}"
-        "${ip4tables} -t filter -D ${input4Rule}"
         "${ip6tables} -t nat -D ${nat6Rule}"
-        "${ip6tables} -t filter -D ${input6Rule}"
       ];
 
-      privateKeyFile = "/nix/secret/wg0.key";
+      privateKeyFile = "/nix/secret/${wgNic}.key";
       generatePrivateKeyFile = true;
 
       peers = [{
